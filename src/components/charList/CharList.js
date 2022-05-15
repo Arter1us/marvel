805a7c7369d6aafde3d3d0e6e5d28240e2750ec1
Arterius <zaghidullinr@bk.ru>
2022-05-15 19:50:05 +0500

    Устранение бага с выбором персонажа

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useMemo } from 'react';
 import Spinner from '../spinner';
 import ErrorMessage from '../errorMessage';
 import useMarvelService from '../../services/MarvelService';
@@ -95,9 +95,14 @@ const CharList = (props) => {
         )
     }
 
+    const elements = useMemo(() => {
+        return setContent(process, () => renderItems(charList), newItemLoading)
+        // eslint-disable-next-line
+    }, [process])
+
     return (
         <div className="char__list">
-            {setContent(process, () => renderItems(charList), newItemLoading)}
+            {elements}
             <button
                 className="button button__main button__long"
                 disabled={newItemLoading}
